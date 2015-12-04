ff4aa5eaffd11a0ca6f2e8537890596d73c96039
Mattias Edin <mattias.edin92@gmail.com>
2015-12-04 11:14:16 +0100

    Added: empatica and retrofit imports

--- app/src/main/java/com/evercalm/evercalmsenses/MainActivity.java
+++ app/src/main/java/com/evercalm/evercalmsenses/MainActivity.java
@@ -3,6 +3,18 @@ package com.evercalm.evercalmsenses;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.empatica.empalink.ConnectionNotAllowedException;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.config.EmpaSensorStatus;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+
+import retrofit.Call;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
